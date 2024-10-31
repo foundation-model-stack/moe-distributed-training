5b34f1383786d87eba833a91beb2d1c2a2f6f73d
Yu Chin Fabian Lim <fabianlim@users.noreply.github.com>
2024-11-01 01:05:55 +0800

    Triton-Only Implementation of ScatterMoE With Expert Parallel (#3)
    * initial draft
    
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>
    
    * fixes
    
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>
    
    * refactoring
    
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>
    
    * added fallbacks for megablocks_ops functions
    
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>
    
    * added fallbacks for megblocks kernels
    
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>
    
    * uodate requirements.txt
    
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>
    
    * fix native megablocks upcast
    
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>
    
    * disable foreach for Dtensor in fsdp1 since we mix regular tensors and dtensors
    
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>
    
    * basic lora support
    
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>
    
    * fix gradient scaling for multiple devices
    
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>
    
    * freeze transformer versions and add flash-attn
    
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>
    
    ---------
    
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>

--- train_fsdp1.py
+++ train_fsdp1.py
@@ -4,22 +4,27 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, HfArgumentParser
 from transformers import TrainingArguments
 from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
 import torch
-from megablocks_utils.shard_moe_utils import shard_moe, get_moe_kwargs
+from fms_accel import prepare_scattemoe
 from transformers.models.mixtral.modeling_mixtral import MixtralSparseMoeBlock
-from megablocks_utils.config_utils import update_mlp_registry
+
+from peft import LoraConfig
+from typing import List
 
 # Demo for running databricks megablocks on mixtral using accelerate + FSDP1
 # - this uses HF Trainer's integration of FSDP1
 MODEL_NAME = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 
-update_mlp_registry()
-
 def main(
+    model_name_or_path=MODEL_NAME,
+    moe_module_name="MixtralSparseMoeBlock",
     max_seq_length=4096,
     load_model_dtype='bfloat16', # FSDP shared params will take 
     attn_implementation='sdpa',
-    use_megablocks_sharding: bool = False,
+    use_scattermoe: bool = False,
+    ep_degree: int = None,
     truncate_model_for_debug: bool = False,
+    lora_r: int = 0,
+    lora_alpha: float = None,
 ):
 
     parser = HfArgumentParser(
@@ -28,7 +33,7 @@ def main(
     training_args, _ = parser.parse_args_into_dataclasses(return_remaining_strings=True)
 
     model = AutoModelForCausalLM.from_pretrained(
-        MODEL_NAME,
+        model_name_or_path,
         torch_dtype=getattr(torch, load_model_dtype), ## UPDATED
         attn_implementation=attn_implementation, ## UPDATED
     )
@@ -39,7 +44,7 @@ def main(
 
     # we set the max sequence length here
     tokenizer = AutoTokenizer.from_pretrained(
-        MODEL_NAME, model_max_length=max_seq_length,
+        model_name_or_path, model_max_length=max_seq_length,
     )
 
     if tokenizer.pad_token is None:
@@ -65,6 +70,31 @@ def main(
         tokenizer=tokenizer, return_tensors='pt'
     )
 
+    peft_config = None
+    if lora_r > 0 and lora_alpha > 0:
+        peft_config = LoraConfig(
+            task_type="CAUSAL_LM", 
+            target_modules='all-linear', # keep it simple for now
+            r=lora_r,
+            lora_alpha=lora_alpha,
+            bias='none', # scattermoe only support no bias
+        )
+
+    if use_scattermoe:
+        # scattermoe to happen before the building the trainer
+        # either at loading or augmentation
+
+        prepare_scattemoe(
+            model, 
+            moe_module_name, 
+            checkpoint_name_or_path=model_name_or_path,
+            rank=torch.distributed.get_rank(),
+            world_size=torch.distributed.get_world_size(),
+            ep_degree=ep_degree,
+            lora_config=peft_config
+        )
+
+
     trainer = SFTTrainer(
         model,
         args=training_args,
@@ -72,28 +102,31 @@ def main(
         formatting_func=formatting_prompts_func,
         max_seq_length=max_seq_length,
         data_collator=data_collator,
+        peft_config=peft_config,
     )
 
-    if use_megablocks_sharding:
+    if use_scattermoe:
 
-        dp_mesh = shard_moe(
-            model, 
-            MixtralSparseMoeBlock, 
-            checkpoint_name_or_path=MODEL_NAME,
-            rank=torch.distributed.get_rank(),
-            world_size=torch.distributed.get_world_size(),
-            ep_size=torch.distributed.get_world_size(),
-            moe_kwargs=get_moe_kwargs(
-                model.config, 
-                has_bias=False,
-                fp16=training_args.fp16,
-                bf16=training_args.bf16,
-            ),
-        )
+        # for newer torch that enables foreach for Dtensors we need to remove it
+        from torch.optim.optimizer import _foreach_supported_types
+
+        i = 0
+        while i < len(_foreach_supported_types):
+            x = _foreach_supported_types[i]
+            if x.__name__ == 'DTensor':
+                _foreach_supported_types.pop(i)
+            else:
+                i += 1 
 
         trainer.accelerator.state.fsdp_plugin.ignored_modules = [
             layer.block_sparse_moe for layer in model.model.layers
         ]
+
+        # NOTE: in the future the quant case because prepare_model_for_kbit_training
+        # is called, this will reset the grads of the ScatterMoe
+        # so we will neeed to set them back to True
+        # - but for regular LoRA this is not needed, because
+        #   _mark_only_adapters_as_trainable will check for prefix
 
     trainer.train()
 
